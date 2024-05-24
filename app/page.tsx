a7037ece2968fab105e741f55243771b1842bbee
Nova Slicer <roythedev.ai@gmail.com>
2024-05-24 21:11:42 +0200

    New update, better navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
+import Aanvraag from "./Components/Aanvraag";
 
 export default function Home() {
   return ( 
     <div>
       <Navbar /> 
       <Hero /> 
+      <Aanvraag /> 
       <About /> 
       <Footer /> 
+ 
     </div>
   );
 }
